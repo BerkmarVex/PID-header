8d43b900e6715e2707faf363c3f7db308ac74526
axiom <karan15547@gmail.com>
2018-01-23 14:49:49 -0500

    added existing files

---
+++ include/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <API.h>
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+    #endif
+
+    void autonomous();
+
+    void initializeIO();
+
+    void initialize();
+
+    #define IME_LEFT_MOTOR 0
+    #define IME_RIGHT_MOTOR 1
+    #define NUMBER_OF_IME 2
+    void operatorControl();
+
+    #ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/opcontrol.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "pid.h"
+
+void operatorControl() {
+	while(1){
+	delay(20);
+	};
+};
